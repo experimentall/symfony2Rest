e72f311c56dfe799ad9c43b469a6f5ab2f08b159
olivier <olivier59@gmail.com>
2013-09-03 18:43:19 +0200

    basic login to access secured content

--- src/Demo/AuthRestBundle/Controller/DefaultController.php
+++ src/Demo/AuthRestBundle/Controller/DefaultController.php
@@ -2,12 +2,36 @@
 
 namespace Demo\AuthRestBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * Secure area.
+     */
+    public function indexAction()
     {
-        return $this->render('DemoAuthRestBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DemoAuthRestBundle:Default:index.html.twig');
     }
+
+
+    public function loginAction(Request $request)
+    {
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'DemoAuthRestBundle:Default:login.html.twig',
+            array(
+                'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
+    }
+
 }
--- src/Demo/AuthRestBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Demo/AuthRestBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,73 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testNotAuthenticated()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $client->request('GET', '/');
+        $crawler = $client->followRedirect();
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("LOGIN")')->count() > 0);
+    }
+
+    public function testBadAuthentication()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+        $crawler = $client->followRedirect();
+
+        $form = $crawler->selectButton('submit_login')->form();
+
+        $form['_username'] = 'bad';
+        $form['_password'] = 'bad';
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('html:contains("LOGIN")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Bad credentials")')->count() > 0);
+    }
+
+    public function testGoodAuthentication()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+        $crawler = $client->followRedirect();
+
+        $form = $crawler->selectButton('submit_login')->form();
+
+        $form['_username'] = 'admin';
+        $form['_password'] = 'adminpass';
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('html:contains("LIST")')->count() > 0);
+    }
+
+    public function testLogout()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+        $crawler = $client->followRedirect();
+
+        $form = $crawler->selectButton('submit_login')->form();
+
+        $form['_username'] = 'admin';
+        $form['_password'] = 'adminpass';
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        $link = $crawler->selectLink('Logout')->link();
+
+        $client->click($link);
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('html:contains("LOGIN")')->count() > 0);
     }
 }
